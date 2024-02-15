6239e8c9c88fb6ea39cab8aca6a35642d01d0824
ImadElMaftouhi <imad.elmaftouhi@etu.uae.ac.ma>
2024-02-15 10:41:53 +0100

    added tuple definiton

---
+++ Python/main.py
@@ -0,0 +1,23 @@
+# All data structure possible using Python 3.12
+
+
+
+"""
+Tuple : A tuple is a python's data structure in the form of an array, the elements are not not mutable (inchangeable). A tuple can hold various variable/constants of different type.
+"""
+
+var = (1,2,3,4,"string",56.34, True) # Same as writing var = () or var = tuple ()
+
+
+# Array/List 
+
+array = [] # same as array = ()
+
+
+# Dictionnary
+    
+dictio = dict() 
+
+
+
+
