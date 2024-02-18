d935b5970c02b643c7c7860f925b014b6c0e2127
ImadElMaftouhi <imad.elmaft.wrk@gmail.com>
2024-02-19 00:34:17 +0100

    added Sll.py
    Library for simple linked list

--- Python/main.py
+++ Python/main.py
@@ -22,56 +22,17 @@ var = [ [1,2], [3,4], [[5,6],[7,8]] ]
 ##linked list 
 
 
-class Sll():
-    
-    list_size = 0 
+import Dll as dll
+import Sll as sll
 
-    def __init__(self, data, next_node=None):
-        self.data = data
-        self.next = next_node
+dllist = dll.Dll()
+for i in range(5) :
+    dllist.dll_append(i)
 
-    def sll_append(self, data):
-        temp = Sll(data)
-        ptr = self
-        while ptr.next is not None: 
-            ptr = ptr.next
-        ptr.next = temp
+dllist.dll_print()
 
-        Sll.list_size+=1        
+sllist = sll.Sll()
+for i in range(5):
+    sllist.sll_append(i)
 
-    def sll_prepend(self, data):
-        temp = Sll(data)
-        if self.next is None:
-            temp.next = self
-            Sll.list_size+=1        
-            return temp
-        temp.next = self
-        Sll.list_size+=1        
-        return temp
-
-
-    def sll_print(self):
-        ptr = self
-        while ptr is not None:
-            print("\n", ptr.data)
-            ptr = ptr.next
-
-
-    def sll_delete(self, rng):
-        ptr = self
-        if rng < 0 or rng >= Sll.list_size:
-            return False
-        for _ in range(rng - 1):
-            ptr = ptr.next
-        ptr.next = ptr.next.next
-        #Deleting a node from our list has to do with python automatic garbage collec@tor
-        #See documentation for more.
-
-
-class dll():
-    list_size = 0
-    def __init__(self, data, next_node = None, prev_node = None):
-        self.next = next_node
-        self.prev = prev_node
-        self.data = data
-        
+sllist.sll_print()
