92a4652d28444e045ed37cba5a8ebbe696805159
ImadElMaftouhi <imad.elmaft.wrk@gmail.com>
2024-02-18 23:53:22 +0100

    added sll functions

--- Python/main.py
+++ Python/main.py
@@ -1,7 +1,5 @@
 # All data structure possible using Python 3.12
 
-
-
 """
 Tuple : A tuple is a python's data structure in the form of an array, the elements are not not mutable (inchangeable). A tuple can hold various variable/constants of different type.
 """
@@ -15,7 +13,6 @@ List : A list is a data strucutre that can hold different data types but are lis
 var = [1,2,3,4,5,6,7,8,9,True, False, 45.3, 12.02] # Same as writing var = [] or var = list()
 
 
-
 """
 Multidimensionnal list :
 """
@@ -26,5 +23,56 @@ var = [ [1,2], [3,4], [[5,6],[7,8]] ]
 
 
 class Sll():
-    def __init__(self, data):
+    
+    list_size = 0 
+
+    def __init__(self, data, next_node=None):
         self.data = data
+        self.next = next_node
+
+    def sll_append(self, data):
+        temp = Sll(data)
+        ptr = self
+        while ptr.next is not None: 
+            ptr = ptr.next
+        ptr.next = temp
+
+        Sll.list_size+=1        
+
+    def sll_prepend(self, data):
+        temp = Sll(data)
+        if self.next is None:
+            temp.next = self
+            Sll.list_size+=1        
+            return temp
+        temp.next = self
+        Sll.list_size+=1        
+        return temp
+
+
+    def sll_print(self):
+        ptr = self
+        while ptr is not None:
+            print("\n", ptr.data)
+            ptr = ptr.next
+
+
+    def sll_delete(self, rng):
+        ptr = self
+        if rng < 0 or rng >= Sll.list_size:
+            return False
+        for _ in range(rng - 1):
+            ptr = ptr.next
+        ptr.next = ptr.next.next
+        #Deleting a node from our list has to do with python automatic garbage collector
+        #See documentation for more.
+       
+
+
+
+head = Sll(0)
+for i in range(1,10) :
+    head.sll_append(i)
+
+
+head.sll_delete(3)
