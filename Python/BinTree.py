344d5fd5b85fd07cc9b790106afc69ac1e6f25ae
ImadElMaftouhi <imad.elmaft.wrk@gmail.com>
2024-02-22 22:12:54 +0100

    added insert()

--- Python/BinTree.py
+++ Python/BinTree.py
@@ -7,3 +7,13 @@ class TreeNode:
         self.data = data
         self.left = None
         self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if not self.root:
+            self.root = TreeNode(data)
+        else:
+            self._insert_recursive(self.root, data)
