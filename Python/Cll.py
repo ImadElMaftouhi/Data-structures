64b195006c15c7c5e764e07532bc68ee1b99af8c
ImadElMaftouhi <imad.elmaft.wrk@gmail.com>
2024-02-23 22:29:57 +0100

    improved Cll.py

--- Python/Cll.py
+++ Python/Cll.py
@@ -1,10 +1,8 @@
 ##########
 ##Circular simple linked list
-
 import Sll as sll
 
-class Cll():
-    
+class Cll:
     def __init__(self):
         self.head = None
         self.size = 0
@@ -14,17 +12,36 @@ class Cll():
         
         if not self.head:
             self.head = new_node
+            new_node.next = new_node  # Make it circular
         else:
             ptr = self.head
-            while ptr.next is not self.head and ptr.next is not None :
+            while ptr.next is not self.head:
                 ptr = ptr.next
             ptr.next = new_node
             new_node.next = self.head
         self.size += 1
         return new_node
                 
-    def cll_print(self, head) -> None:
-        print("\n", head.data)
-        if ( head.next is not self.head and head is not None ) :
-            self.cll_print(head.next)
+    def cll_print(self):
+        if not self.head:
+            print("Circular linked list is empty")
+            return
         
+        ptr = self.head
+        while True:
+            print(ptr.data)
+            ptr = ptr.next
+            if ptr is self.head:
+                break
+
+# def main():
+#     cllist = Cll()
+#     cllist.cll_append(1)
+#     cllist.cll_append(2)
+#     cllist.cll_append(3)
+
+#     print("Circular linked list:")
+#     cllist.cll_print()
+
+# if __name__ == "__main__":
+#     main()
