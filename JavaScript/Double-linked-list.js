b6a7eed48b5415f8c0988bcee8a56bbff09eb656
ImadElMaftouhi <imad.elmaft.wrk@gmail.com>
2024-03-06 23:55:54 +0100

    added insert() method

--- JavaScript/Double-linked-list.js
+++ JavaScript/Double-linked-list.js
@@ -25,4 +25,32 @@ class DoublyLinkedList {
           this.tail = newNode;
         }
         this.size++;
-      }
+    }
+
+    insertAt(element, index) {
+    if (index < 0 || index > this.size) {
+        return false;
+    } else {
+        const newNode = new Node(element);
+        let current, previous;
+        current = this.head;
+        if (index === 0) {
+        newNode.next = this.head;
+        this.head.prev = newNode;
+        this.head = newNode;
+        } else {
+        let i = 0;
+        while (i < index) {
+            i++;
+            previous = current;
+            current = current.next;
+        }
+        newNode.next = current;
+        newNode.prev = previous;
+        current.prev = newNode;
+        previous.next = newNode;
+        }
+        this.size++;
+        return true;
+    }
+    }
